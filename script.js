d952e19492e957bf9648b65d04829ecb6877d7ef
Proxy_Kad <proxykad59@gmail.com>
2025-11-30 01:49:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -111,3 +111,27 @@ function animateCursor() {
 }
 
 animateCursor();
+
+
+/*-----
+Gallery script
+-----*/
+
+const track = document.getElementById("galleryTrack");
+
+// Duplicate the images for seamless loop
+track.innerHTML += track.innerHTML;
+
+let speed = 0.5; // pixels per frame
+let pos = 0;
+
+function animateGallery() {
+  pos -= speed;
+  if (pos <= -track.scrollWidth / 2) {
+    pos = 0; // reset to start seamlessly
+  }
+  track.style.transform = `translateX(${pos}px)`;
+  requestAnimationFrame(animateGallery);
+}
+
+animateGallery();
